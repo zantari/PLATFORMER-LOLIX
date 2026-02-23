b2cbdf92695ef190c60ebc26c911511c1189956d
zantari <ewaind8@gmail.com>
2026-02-23 08:12:58 +0100

    s

--- scenes/level_3.gd
+++ scenes/level_3.gd
@@ -3,22 +3,7 @@ extends Node2D
 const bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
 
 func _ready() -> void:
-	# 1. REFERENCJA DO GRACZA
-	var player = get_tree().get_first_node_in_group("Player")
-	
-	if player:
-		player.can_use_shield = true
-		
-		# 2. USTAWIANIE POZYCJI (ROZWIĄZANIE TWOJEGO PROBLEMU)
-		# Szukamy punktu o nazwie MenuStart (Marker2D), który postawiłeś na mapie
-		var spawn_point = get_node_or_null("MenuStart")
-		if spawn_point:
-			player.global_position = spawn_point.global_position
-		else:
-			# Jeśli nie ma punktu MenuStart, używamy współrzędnych z Global
-			player.global_position = Global.spawn_position
-
-	# --- RESZTA TWOJEJ LOGIKI ---
+	get_tree().get_first_node_in_group("Player").can_use_shield = true
 	get_tree().get_first_node_in_group("diamondLabel").maxDiamenty = 5
 	Global.temporary_diamonds = 0 
 	Global.currentScene = "level2"
@@ -32,3 +17,8 @@ func _on_player_shoot(pos, facing_right) -> void:
 	bullet.direction = direction
 	$Bullets.add_child(bullet)
 	bullet.position = pos + Vector2(14 * direction, 5)
+
+
+func _on_teleport_body_entered(body: Node2D) -> void:
+	print("wyjebalo poza mape")
+	body.global_position = Vector2(2518.0, 923.0)
